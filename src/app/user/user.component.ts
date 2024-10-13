adef99c2c1bddf1d3009a8643f02c813be02a3f3
Daniel Varela <dgvarelamartinez@gmail.com>
2024-10-13 22:21:12 +0000

    events

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,4 +11,14 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 })
 export class UserComponent {
   selectedUser = DUMMY_USERS[randomIndex];
+
+  get imagePath(){
+    return 'assets/users/' + this.selectedUser.avatar
+  }
+
+  onSelectUser(){
+
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+    this.selectedUser = DUMMY_USERS[randomIndex];
+  }
 }
