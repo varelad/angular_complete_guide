2878e6a290ffc0402cd9b617524cce2b1e0b4406
Daniel Varela <dgvarelamartinez@gmail.com>
2024-10-14 23:32:31 +0000

    input component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +12,6 @@ import { UserComponent } from './user/user.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  users = DUMMY_USERS;
   title = 'angular_complete_guide';
 }
