19b231a59bbd427e3eb5b1c9996ff2dce668ea0f
Daniel Varela <dgvarelamartinez@gmail.com>
2024-10-15 23:38:36 +0000

    tasks showing name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,26 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   users = DUMMY_USERS;
   title = 'angular_complete_guide';
+  selectedUserId = 'u1';
+
+  get selectedUser(){
+    return this.users.find( (user) => user.id === this.selectedUserId )!;
+  }
 
   onSelectUser(id: string){
     console.log('Selected  user with ' + id);
+    this.selectedUserId = id;
   }
 }
